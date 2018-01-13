a2fe24c4542ddb94458695bec6a76bbaa29ac547
Elias Boiko <elias.boiko@gmail.com>
2018-01-13 02:12:33 +0200

    1

--- Homework 13 jan 18/Program.cs
+++ Homework 13 jan 18/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace Homework_13_jan_18
 {
+    static class ConvertTemp
+    {
+        public static double FtoK(double temperature)
+        {
+            return (temperature + 459.67) * 5 / 9;
+        }
+        public static double KtoF(double temperature)
+        {
+            return (temperature * 9 / 5) - 459.67;
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            double mytemperature = double.Parse(Console.ReadLine());
+            Console.WriteLine(ConvertTemp.FtoK(mytemperature));
+            Console.WriteLine(ConvertTemp.KtoF(mytemperature));
         }
     }
 }
